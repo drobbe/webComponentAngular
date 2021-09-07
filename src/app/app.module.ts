7ad57a16e71a9120dcc269de2ebafc7cfa1d97a8
edgard1394@gmail.com <edgard1394@gmail.com>
2021-09-07 16:03:10 -0500

    gg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WebphoneComponent } from './webphone/webphone.component';
+import { Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [WebphoneComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [WebphoneComponent],
+  entryComponents: [WebphoneComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(WebphoneComponent, { injector });
+    customElements.define('news-widget', el);
+  }
+  ngDoBootstrap() {}
+}
